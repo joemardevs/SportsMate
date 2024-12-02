91884ffa09ef255fd07a8469655e0d06b6d631ea
Joemar Palting <joemarpalting19@gmail.com>
2024-12-02 23:10:26 +0800

    account setup interested in ui

--- components/AccountSetupScreen/interested-in.tsx
+++ components/AccountSetupScreen/interested-in.tsx
@@ -3,6 +3,21 @@ import React from "react";
 import { AccountSetupProps } from "@/app/(sign-up)/account-setup";
 import AccountSetupContainer from "./components/AccountSetupContainer";
 
+const OPTIONS = [
+  {
+    label: "Women",
+    value: "women",
+  },
+  {
+    label: "Men",
+    value: "men",
+  },
+  {
+    label: "All",
+    value: "all",
+  },
+];
+
 const InterestedInAccountSetup = ({
   onNext,
   isContinueButtonDisabled,
@@ -11,8 +26,29 @@ const InterestedInAccountSetup = ({
     <AccountSetupContainer
       onNext={onNext}
       isContinueButtonDisabled={isContinueButtonDisabled}>
-      <View className="flex-1 w-full flex justify-center items-center">
-        <Text className="text-2xl text-white font-sfbold">Interested In</Text>
+      <View className="flex-1">
+        <Text className="text-2xl text-white font-sfmedium">
+          Who are you interested in?
+        </Text>
+
+        <Text className="text-base text-white font-sfregular mt-4">
+          You can select as many as you want
+        </Text>
+
+        <View className="flex flex-col gap-4 mt-6">
+          {OPTIONS.map(option => (
+            <View
+              key={option.value}
+              className="flex flex-row items-center w-1/2">
+              <View className="bg-white w-6 h-6 rounded-full mr-4" />
+              <View className="px-4 py-2 bg-white w-full rounded-full">
+                <Text className="text-base text-black font-sfregular text-center">
+                  {option.label}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </View>
       </View>
     </AccountSetupContainer>
   );
