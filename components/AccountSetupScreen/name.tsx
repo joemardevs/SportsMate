37bf5d74275625be44120c69d7d27fec7757eaab
Joemar Palting <joemarpalting19@gmail.com>
2024-12-02 22:16:57 +0800

    account setup name ui

--- components/AccountSetupScreen/name.tsx
+++ components/AccountSetupScreen/name.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { AccountSetupProps } from "@/app/(sign-up)/account-setup";
 import AccountSetupContainer from "./components/AccountSetupContainer";
+import Input from "../Input";
 
 const NameAccountSetup = ({
   onNext,
@@ -11,8 +12,17 @@ const NameAccountSetup = ({
     <AccountSetupContainer
       onNext={onNext}
       isContinueButtonDisabled={isContinueButtonDisabled}>
-      <View className="flex-1 w-full flex justify-center items-center">
-        <Text className="text-2xl text-white font-sfbold">Name</Text>
+      <View className="flex-1">
+        <Text className="text-2xl text-white font-sfmedium">
+          What's your name?
+        </Text>
+
+        <Input
+          title=""
+          placeholder="Enter your name"
+          containerStyle="mt-4"
+          handleChangeText={text => console.log("Name:", text)}
+        />
       </View>
     </AccountSetupContainer>
   );
