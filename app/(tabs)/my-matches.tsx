e5d8f6c160a5b94486749d83c8fd14526bc196e6
Joemar Palting <joemarpalting19@gmail.com>
2025-01-14 22:42:21 +0800

    (fix): add initial my matches screen

--- app/(tabs)/my-matches.tsx
+++ app/(tabs)/my-matches.tsx
@@ -1,13 +1,25 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Platform, StatusBar } from "react-native";
 import React from "react";
 
 const MyMatchesScreen = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text className="text-black">My Matches</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        className="flex flex-row items-center justify-center px-2 py-4 rounded-b-[30px] bg-secondary "
+        style={{
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 70,
+        }}>
+        <Text className="text-white text-xl font-bold">My Matches</Text>
       </View>
-    </SafeAreaView>
+
+      <SafeAreaView className="flex-1 bg-white">
+        <View className="flex-1 w-full flex justify-center items-center">
+          <Text className="text-black text-2xl font-bold">
+            My Matches Screen
+          </Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
