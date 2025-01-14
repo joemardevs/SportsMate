571229ed3f280c5a3b8fc0c90d661a6335e3bf0c
Joemar Palting <joemarpalting19@gmail.com>
2025-01-14 22:47:12 +0800

    (fix): add initial chat screen

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,13 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Platform, StatusBar } from "react-native";
 import React from "react";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const ChatScreen = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text className="text-black">Chat</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        className="flex flex-row items-center justify-between px-2 py-4 rounded-b-[30px] bg-secondary "
+        style={{
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 70,
+        }}>
+        <View className="bg-white p-2 rounded-full">
+          <Feather name="search" size={32} color="#f0566a" />
+        </View>
+        <Text className="text-white text-xl font-bold">Chat</Text>
+        <View className="bg-primary p-2 rounded-full">
+          <MaterialCommunityIcons name="pencil" size={32} color="white" />
+        </View>
       </View>
-    </SafeAreaView>
+
+      <SafeAreaView className="flex-1 bg-white">
+        <View className="flex-1 w-full flex justify-center items-center">
+          <Text className="text-black text-2xl font-bold">Chat Screen</Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
